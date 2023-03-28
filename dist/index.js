8d09b4f9c7864a03961cc4a77b1cc2da0706da10
yuki-uky <yuki.ukyy@gmail.com>
2023-03-28 20:24:31 +0100

    Project initialization

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+tippy("#copy-button", {
+    content: "Copy",
+    delay: [600, 250]
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+declare const tippy: any;
+
+tippy("#copy-button", {
+    content: "Copy",
+    delay: [600, 250]
+});
