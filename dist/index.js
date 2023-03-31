928c070c36a5f4c956e29909fefe8ce92d9bc034
yuki-uky <yuki.ukyy@gmail.com>
2023-03-31 23:39:07 +0100

    App ui and logics are being added

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,30 @@
 "use strict";
-tippy("#copy-button", {
-    content: "Copy",
-    delay: [600, 250]
-});
+const characterLengthInput = document.getElementById('charater-length-input'), progressBar = document.getElementById('range-progress'), checkboxInputs = document.querySelectorAll('input[type="checkbox"]'), characterLengthCounter = document.getElementById('character-length-counter');
+class password {
+    constructor() {
+        this.result = "";
+        this.length = 0;
+        this.upperCase = false;
+        this.lowerCase = false;
+        this.numbers = false;
+        this.symbols = false;
+    }
+    setCharacterLength(value) {
+        this.length = value;
+        characterLengthCounter.innerText = this.length.toString();
+    }
+    toggleOptions(property) {
+        this[property] = !this[property];
+    }
+}
+const mainPassword = new password();
+function handleInput(e) {
+    progressBar.value = e.target.value;
+    mainPassword.setCharacterLength(parseInt(e.target.value));
+}
+function handleCheckbox(e) {
+    mainPassword.toggleOptions(e.target.value);
+}
+characterLengthInput.addEventListener("input", handleInput);
+checkboxInputs.forEach(checkbox => checkbox.addEventListener('input', handleCheckbox));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,60 @@
-declare const tippy: any;
+const characterLengthInput = <HTMLInputElement>document.getElementById('charater-length-input'), progressBar = <HTMLInputElement>document.getElementById('range-progress'),
+    checkboxInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[type="checkbox"]'),
+    characterLengthCounter = <HTMLHeadingElement>document.getElementById('character-length-counter');
 
-tippy("#copy-button", {
-    content: "Copy",
-    delay: [600, 250]
-});
+interface PasswordProperties {
+    length: number,
+    upperCase: boolean,
+    lowerCase: boolean,
+    numbers: boolean,
+    symbols: boolean
+}
+
+type ToggleProperty = 'upperCase' | 'lowerCase' | 'numbers' | 'symbols';
+
+interface Password extends PasswordProperties {
+    result: string,
+    setCharacterLength: (value: number) => void,
+    toggleOptions: (property: ToggleProperty) => void
+}
+
+class password implements Password {
+    result: string;
+    length: number;
+    upperCase: boolean;
+    lowerCase: boolean;
+    numbers: boolean;
+    symbols: boolean;
+
+    constructor() {
+        this.result = "";
+        this.length = 0;
+        this.upperCase = false;
+        this.lowerCase = false;
+        this.numbers = false;
+        this.symbols = false;
+    }
+
+    setCharacterLength(value: number) {
+        this.length = value;
+        characterLengthCounter.innerText = this.length.toString();
+    }
+
+    toggleOptions(property: ToggleProperty) {
+        this[property] = !this[property];
+    }
+}
+
+const mainPassword = new password();
+
+
+function handleInput(e) {
+    progressBar.value = e.target.value;
+    mainPassword.setCharacterLength(parseInt(e.target.value));
+}
+function handleCheckbox(e) {
+    mainPassword.toggleOptions(e.target.value);
+}
+
+characterLengthInput.addEventListener("input", handleInput);
+checkboxInputs.forEach(checkbox => checkbox.addEventListener('input', handleCheckbox))
