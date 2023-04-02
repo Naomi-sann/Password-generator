25d1502de2031f8d3a2cb1dba9cbaf9e85d34744
yuki-uky <yuki.ukyy@gmail.com>
2023-04-02 20:30:35 +0100

    Everything's done despite the strength counter

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,25 @@
 "use strict";
-const characterLengthInput = document.getElementById('charater-length-input'), progressBar = document.getElementById('range-progress'), checkboxInputs = document.querySelectorAll('input[type="checkbox"]'), characterLengthCounter = document.getElementById('character-length-counter');
+const [copyTippy] = tippy('#copy-button', {
+    content: "Copy",
+    delay: [300, 600],
+    hideOnClick: false,
+    onHidden(instance) {
+        instance.setContent("Copy");
+    },
+    theme: "light"
+});
+const characterLengthInput = document.getElementById('charater-length-input'), progressBar = document.getElementById('range-progress'), checkboxInputs = document.querySelectorAll('input[type="checkbox"]'), characterLengthCounter = document.getElementById('character-length-counter'), generateButton = document.getElementById('generate'), resultTitle = document.getElementById('result'), copyButton = document.getElementById('copy-button');
+var Strength;
+(function (Strength) {
+    Strength[Strength["empty"] = 0] = "empty";
+    Strength[Strength["easy"] = 1] = "easy";
+    Strength[Strength["medium"] = 2] = "medium";
+    Strength[Strength["strong"] = 3] = "strong";
+})(Strength || (Strength = {}));
 class password {
     constructor() {
         this.result = "";
+        this.strength = Strength.empty;
         this.length = 0;
         this.upperCase = false;
         this.lowerCase = false;
@@ -16,6 +33,26 @@ class password {
     toggleOptions(property) {
         this[property] = !this[property];
     }
+    generatePassword() {
+        const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lowerCaseLetters = upperCaseLetters.toLowerCase();
+        const numbers = "1234567890";
+        const symbols = "!@#$%^&*-_+=";
+        const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+        const availableCharGroups = [];
+        this.upperCase && availableCharGroups.push(upperCaseLetters);
+        this.lowerCase && availableCharGroups.push(lowerCaseLetters);
+        this.numbers && availableCharGroups.push(numbers);
+        this.symbols && availableCharGroups.push(symbols);
+        this.result = "";
+        if (!availableCharGroups.length)
+            return "";
+        for (let i = 0; i < this.length; i++) {
+            const randomCharacterGroup = availableCharGroups[random(0, availableCharGroups.length - 1)];
+            this.result += randomCharacterGroup.charAt(random(0, randomCharacterGroup.length - 1));
+        }
+        return this.result;
+    }
 }
 const mainPassword = new password();
 function handleInput(e) {
@@ -25,6 +62,22 @@ function handleInput(e) {
 function handleCheckbox(e) {
     mainPassword.toggleOptions(e.target.value);
 }
+function handleGenerate() {
+    const generatedPassword = mainPassword.generatePassword();
+    if (generatedPassword !== "") {
+        resultTitle.innerText = generatedPassword;
+        resultTitle.className = "";
+    }
+    else {
+        resultTitle.innerText = "Empty";
+        resultTitle.className = "empty-password";
+    }
+}
+function handleCopy() {
+    navigator.clipboard.writeText(mainPassword.result).then(() => copyTippy.setContent("Copied")).catch(() => copyTippy.setContent("Could not copy!"));
+}
 characterLengthInput.addEventListener("input", handleInput);
 checkboxInputs.forEach(checkbox => checkbox.addEventListener('input', handleCheckbox));
+generateButton.addEventListener("click", handleGenerate);
+copyButton.addEventListener("click", handleCopy);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+declare const tippy: any;
+
+const [copyTippy] = tippy('#copy-button', {
+    content: "Copy",
+    delay: [300, 600],
+    hideOnClick: false,
+    onHidden(instance) {
+        instance.setContent("Copy")
+    },
+    theme: "light"
+});
+
 const characterLengthInput = <HTMLInputElement>document.getElementById('charater-length-input'), progressBar = <HTMLInputElement>document.getElementById('range-progress'),
     checkboxInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[type="checkbox"]'),
-    characterLengthCounter = <HTMLHeadingElement>document.getElementById('character-length-counter');
+    characterLengthCounter = <HTMLHeadingElement>document.getElementById('character-length-counter'),
+    generateButton = <HTMLButtonElement>document.getElementById('generate'),
+    resultTitle = <HTMLHeadingElement>document.getElementById('result'),
+    copyButton = <HTMLButtonElement>document.getElementById('copy-button');
 
 interface PasswordProperties {
     length: number,
@@ -18,8 +33,16 @@ interface Password extends PasswordProperties {
     toggleOptions: (property: ToggleProperty) => void
 }
 
+enum Strength {
+    empty = 0,
+    easy = 1,
+    medium = 2,
+    strong = 3,
+}
+
 class password implements Password {
     result: string;
+    strength: Strength;
     length: number;
     upperCase: boolean;
     lowerCase: boolean;
@@ -28,6 +51,7 @@ class password implements Password {
 
     constructor() {
         this.result = "";
+        this.strength = Strength.empty;
         this.length = 0;
         this.upperCase = false;
         this.lowerCase = false;
@@ -35,18 +59,46 @@ class password implements Password {
         this.symbols = false;
     }
 
-    setCharacterLength(value: number) {
+    setCharacterLength(value: number): void {
         this.length = value;
         characterLengthCounter.innerText = this.length.toString();
     }
 
-    toggleOptions(property: ToggleProperty) {
+    toggleOptions(property: ToggleProperty): void {
         this[property] = !this[property];
+    }
+
+    generatePassword(): string {
+        const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lowerCaseLetters = upperCaseLetters.toLowerCase();
+        const numbers = "1234567890";
+        const symbols = "!@#$%^&*-_+=";
+
+        const random = (min: number, max: number): number => Math.floor(Math.random() * (max - min + 1) + min);
+
+        const availableCharGroups: string[] = [];
+
+        this.upperCase && availableCharGroups.push(upperCaseLetters);
+        this.lowerCase && availableCharGroups.push(lowerCaseLetters);
+        this.numbers && availableCharGroups.push(numbers);
+        this.symbols && availableCharGroups.push(symbols);
+
+        this.result = "";
+
+        if (!availableCharGroups.length) return "";
+
+        for (let i = 0; i < this.length; i++) {
+            const randomCharacterGroup: string = availableCharGroups[random(0, availableCharGroups.length - 1)];
+
+
+            this.result += randomCharacterGroup.charAt(random(0, randomCharacterGroup.length - 1));
+        }
+
+        return this.result;
     }
 }
 
-const mainPassword = new password();
-
+const mainPassword: password = new password();
 
 function handleInput(e) {
     progressBar.value = e.target.value;
@@ -55,6 +107,28 @@ function handleInput(e) {
 function handleCheckbox(e) {
     mainPassword.toggleOptions(e.target.value);
 }
+function handleGenerate() {
+    const generatedPassword = mainPassword.generatePassword();
+
+    if (generatedPassword !== "") {
+        resultTitle.innerText = generatedPassword;
+        resultTitle.className = "";
+    }
+    else {
+        resultTitle.innerText = "Empty";
+        resultTitle.className = "empty-password";
+    }
+}
+function handleCopy() {
+    navigator.clipboard.writeText(mainPassword.result).then(() =>
+        copyTippy.setContent("Copied")
+    ).catch(() =>
+        copyTippy.setContent("Could not copy!")
+    );
+
+}
 
 characterLengthInput.addEventListener("input", handleInput);
-checkboxInputs.forEach(checkbox => checkbox.addEventListener('input', handleCheckbox))
+checkboxInputs.forEach(checkbox => checkbox.addEventListener('input', handleCheckbox));
+generateButton.addEventListener("click", handleGenerate);
+copyButton.addEventListener("click", handleCopy);
